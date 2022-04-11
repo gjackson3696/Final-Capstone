7d469807327d90a1ea0ad384103d294e73756e67
Gary Jackson <gjackson3696@gmail.com>
2022-04-11 09:21:57 -0400

    More Dao and model work.

--- java/src/main/java/com/techelevator/dao/ClassDao.java
+++ java/src/main/java/com/techelevator/dao/ClassDao.java
@@ -1,4 +1,19 @@
 package com.techelevator.dao;
 
+import com.techelevator.model.Class;
+
+import java.util.List;
+
 public interface ClassDao {
+
+    List<Class> findAll();
+
+    Class getClassById(Long id);
+
+    boolean addClass(String name, String instructor, String time, String date, int lengthMinutes);
+
+    boolean updateClass(Class updatedClass);
+
+    boolean removeClass(Long id);
+
 }
