00d21acdba165368a9338238f8d70a4e388990ea
Alex Lozano <59184918+AlexanderLozano162017@users.noreply.github.com>
2022-04-11 12:19:22 -0700

    push from sunday

---
+++ vue/src/components/Class.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+<p>{{ classItem.id }} {{ classItem.name }}  {{ classItem.instructor }}  {{ classItem.date }}  {{ classItem.startTime }} {{ classItem.lengthInMinuets }} </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "schedule",
+  data() {
+    return {};
+  },
+  props: [ 'classItem' ]
+};
+</script>
+
+<style></style>
