956db2f54ab6d938f3b2f9c9c84a1539414d75e0
Gary Jackson <gjackson3696@gmail.com>
2022-04-13 00:13:49 -0400

    Back end mostly finished for sprint

--- java/src/main/java/com/techelevator/dao/ClassDao.java
+++ java/src/main/java/com/techelevator/dao/ClassDao.java
@@ -19,4 +19,7 @@ public interface ClassDao {
 
     Class removeClass(Long id) throws ClassNotFoundException;
 
+    void registerMember(Long memberId, Long classId);
+
+    void unregisterMember(Long memberId, Long classId);
 }
