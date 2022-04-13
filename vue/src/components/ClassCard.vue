796894d08a5b3fcadd792604fbff781d229460bb
Alex Lozano <59184918+AlexanderLozano162017@users.noreply.github.com>
2022-04-13 06:11:47 -0700

    button demo funtion added

--- vue/src/components/ClassCard.vue
+++ vue/src/components/ClassCard.vue
@@ -6,8 +6,10 @@
           <p class="class-items">Instructor: {{ classItem.instructor }}  </p>
           <p class="class-items">Start Time: {{ classItem.time }} </p>
           <p class="class-items">Length of Class: {{ classItem.lengthMinutes }} mins.</p>
-          <button>Register for Class</button>
-    
+        <div >
+            <!-- add dynamic styling to the on click event to toggle between registered and unregister class="lightView, base-button, darkView" -->
+            <button  @click="changeText">{{ button.text }}</button>
+        </div>
     </div>
   </div>
 </template>
@@ -16,23 +18,48 @@
 export default {
   name: "schedule",
   data() {
-    return {};
+    return {
+      changeBtn: false,
+      button: {
+        text: 'Register For Class'
+      },
+    }
+  },
+      methods: {
+      changeText: function() {
+        this.changeBtn = !this.changeBtn;
+        this.button.text = this.changeBtn ? 'Register For Class' : 'Unregister Me';
+      }
   },
 
-  props: [ 'classItem' ],
+  props: [ 'classItem' ], 
   
-
 };
 </script>
 
 <style>
 .class-card-container {
   border: 5px solid rgb(90, 153, 255);
-  border-radius: 10px;
+  border-radius: 20px;
   width: 250px;
   height: 270px;
   margin:10px;
   text-align: center;
+}
+
+.base-button{
+  padding: 20px;
+  border-color: red;
+  border-radius: 20px solid black;
+  }
+
+.lightView {
+  background-color: aquamarine;
+  color: black;
+}
+.darkView {
+  background-color: blueviolet;
+  color: azure;
 }
 
 </style>
--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -36,6 +36,7 @@
       <button class="btn btn-lg btn-primary btn-block" type="submit">
         Create Account
       </button>
+
     </form>
   </div>
 </template>
