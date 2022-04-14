99d6fdc702e81d2580865f6ab10cb168b277fa20
Gary Jackson <gjackson3696@gmail.com>
2022-04-13 22:03:38 -0400

    Added some functionality to the back end

--- vue/src/services/ClassService.js
+++ vue/src/services/ClassService.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 export default {
     getClasses () {
-        return axios.get('/classes');
+        return axios.get('/classes',{
+            "startDate": "2022-04-24"
+        });
     }
 }
