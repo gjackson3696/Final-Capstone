c98b341d2e093d84da3f29601cd836f65ecd7787
Luke Thompson <coachluke@gmail.com>
2022-04-18 19:28:57 -0400

    More pretties and stubbed out component/view for workout logger.

---
+++ vue/src/views/WorkoutLogger.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>Log Workouts</h2>
+    <log-workout />
+  </div>
+</template>
+
+<script>
+import LogWorkout from '../components/LogWorkout.vue'
+export default {
+  components: { LogWorkout },
+  name: "log-workout"
+}
+</script>
+
+<style>
+
+</style>
